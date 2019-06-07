9a17dad4edbb2eaed8de9ef9a0951088ca97d9f1
Mark Lawrence <grumpycoder@outlook.com>
2019-06-06 21:07:44 -0500

    setup for spa routing

--- NgCourses/App_Start/RouteConfig.cs
+++ NgCourses/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace NgCourses
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "spa-route", url: "{*url}",
+                defaults: new { controller = "Home", action = "Index" }
+                );
+
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
